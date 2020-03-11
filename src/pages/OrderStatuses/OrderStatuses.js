0767e7c805baa3ef79db2792a256266c75b7767c
Mohamed Bin Othman <binothman.mo@gmail.com>
2020-03-11 19:25:25 +0300

    Update OrderStatuses.js
    add result to the form

--- src/pages/OrderStatuses/OrderStatuses.js
+++ src/pages/OrderStatuses/OrderStatuses.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Form from './Form';
+import Form from "./Form";
 
-const OrderStatuses = ({
-  handleOnSubmit,
-  handleAddNewStatus,
-}) => (
+const OrderStatuses = ({ handleOnSubmit, handleAddNewStatus, values }) => (
   <Form
     onSubmit={handleOnSubmit}
     handleAddNewStatus={handleAddNewStatus}
+    results={values}
   />
 );
 
 OrderStatuses.propTypes = {
   handleOnSubmit: PropTypes.func,
-  handleAddNewStatus: PropTypes.func,
+  handleAddNewStatus: PropTypes.func
 };
 
 export default OrderStatuses;
